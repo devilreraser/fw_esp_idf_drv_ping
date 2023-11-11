40f9d99d9e442bb4cd75e3ba516268ea76322a8c
Dimitar Lilov <devilerser@gmail.com>
2023-11-11 19:12:24 +0200

    Created based on endpoint project

---
+++ cmd_ping.c
@@ -0,0 +1,237 @@
+/* *****************************************************************************
+ * File:   cmd_ping.c
+ * Author: Dimitar Lilov
+ *
+ * Created on 2022 06 18
+ * 
+ * Description: ...
+ * 
+ **************************************************************************** */
+
+/* *****************************************************************************
+ * Header Includes
+ **************************************************************************** */
+#include "cmd_ping.h"
+#include "drv_ping.h"
+
+#include <string.h>
+
+#include "esp_log.h"
+#include "esp_console.h"
+#include "esp_system.h"
+
+#include "lwip/inet.h"
+#include "lwip/netdb.h"
+#include "lwip/sockets.h"
+
+#include "argtable3/argtable3.h"
+
+#include "ping/ping_sock.h"
+
+#include "drv_console_if.h"
+
+
+/* *****************************************************************************
+ * Configuration Definitions
+ **************************************************************************** */
+#define TAG "cmd_ping"
+
+/* *****************************************************************************
+ * Constants and Macros Definitions
+ **************************************************************************** */
+
+/* *****************************************************************************
+ * Enumeration Definitions
+ **************************************************************************** */
+
+/* *****************************************************************************
+ * Type Definitions
+ **************************************************************************** */
+
+/* *****************************************************************************
+ * Function-Like Macros
+ **************************************************************************** */
+
+/* *****************************************************************************
+ * Variables Definitions
+ **************************************************************************** */
+
+static struct {
+    struct arg_dbl *timeout;
+    struct arg_dbl *interval;
+    struct arg_int *data_size;
+    struct arg_int *count;
+    struct arg_int *tos;
+    struct arg_str *host;
+    struct arg_end *end;
+} ping_args;
+
+/* *****************************************************************************
+ * Prototype of functions definitions
+ **************************************************************************** */
+
+/* *****************************************************************************
+ * Functions
+ **************************************************************************** */
+
+
+static void cmd_ping_on_ping_success(esp_ping_handle_t hdl, void *args)
+{
+    uint8_t ttl;
+    uint16_t seqno;
+    uint32_t elapsed_time, recv_len;
+    ip_addr_t target_addr;
+    esp_ping_get_profile(hdl, ESP_PING_PROF_SEQNO, &seqno, sizeof(seqno));
+    esp_ping_get_profile(hdl, ESP_PING_PROF_TTL, &ttl, sizeof(ttl));
+    esp_ping_get_profile(hdl, ESP_PING_PROF_IPADDR, &target_addr, sizeof(target_addr));
+    esp_ping_get_profile(hdl, ESP_PING_PROF_SIZE, &recv_len, sizeof(recv_len));
+    esp_ping_get_profile(hdl, ESP_PING_PROF_TIMEGAP, &elapsed_time, sizeof(elapsed_time));
+    printf("%" PRIu32 " bytes from %s icmp_seq=%d ttl=%d time=%" PRIu32 " ms\n",
+           recv_len, ipaddr_ntoa((ip_addr_t*)&target_addr), seqno, ttl, elapsed_time);
+}
+
+static void cmd_ping_on_ping_timeout(esp_ping_handle_t hdl, void *args)
+{
+    uint16_t seqno;
+    ip_addr_t target_addr;
+    esp_ping_get_profile(hdl, ESP_PING_PROF_SEQNO, &seqno, sizeof(seqno));
+    esp_ping_get_profile(hdl, ESP_PING_PROF_IPADDR, &target_addr, sizeof(target_addr));
+    printf("From %s icmp_seq=%d timeout\n",ipaddr_ntoa((ip_addr_t*)&target_addr), seqno);
+}
+
+static void cmd_ping_on_ping_end(esp_ping_handle_t hdl, void *args)
+{
+    ip_addr_t target_addr;
+    uint32_t transmitted;
+    uint32_t received;
+    uint32_t total_time_ms;
+    esp_ping_get_profile(hdl, ESP_PING_PROF_REQUEST, &transmitted, sizeof(transmitted));
+    esp_ping_get_profile(hdl, ESP_PING_PROF_REPLY, &received, sizeof(received));
+    esp_ping_get_profile(hdl, ESP_PING_PROF_IPADDR, &target_addr, sizeof(target_addr));
+    esp_ping_get_profile(hdl, ESP_PING_PROF_DURATION, &total_time_ms, sizeof(total_time_ms));
+    uint32_t loss = (uint32_t)((1 - ((float)received) / transmitted) * 100);
+    if (IP_IS_V4(&target_addr)) {
+        printf("\n--- %s ping statistics ---\n", inet_ntoa(*ip_2_ip4(&target_addr)));
+    } else {
+        printf("\n--- %s ping statistics ---\n", inet6_ntoa(*ip_2_ip6(&target_addr)));
+    }
+    printf("%" PRIu32 " packets transmitted, %" PRIu32 " received, %" PRIu32 "%% packet loss, time %" PRIu32 "ms\n",
+           transmitted, received, loss, total_time_ms);
+    // delete the ping sessions, so that we clean up all resources and can create a new ping session
+    // we don't have to call delete function in the callback, instead we can call delete function from other tasks
+    esp_ping_delete_session(hdl);
+}
+
+
+
+static int do_ping_cmd(int argc, char **argv)
+{
+    drv_console_set_log_disabled();
+    
+
+    esp_ping_config_t config = ESP_PING_DEFAULT_CONFIG();
+
+    int nerrors = arg_parse(argc, argv, (void **)&ping_args);
+    if (nerrors != 0) {
+        arg_print_errors(stderr, ping_args.end, argv[0]);
+        return 1;
+    }
+
+    if (ping_args.timeout->count > 0) {
+        config.timeout_ms = (uint32_t)(ping_args.timeout->dval[0] * 1000);
+    }
+
+    if (ping_args.interval->count > 0) {
+        config.interval_ms = (uint32_t)(ping_args.interval->dval[0] * 1000);
+    }
+
+    if (ping_args.data_size->count > 0) {
+        config.data_size = (uint32_t)(ping_args.data_size->ival[0]);
+    }
+
+    if (ping_args.count->count > 0) {
+        config.count = (uint32_t)(ping_args.count->ival[0]);
+    }
+
+    if (ping_args.tos->count > 0) {
+        config.tos = (uint32_t)(ping_args.tos->ival[0]);
+    }
+
+    // parse IP address
+    struct sockaddr_in6 sock_addr6;
+    ip_addr_t target_addr;
+    memset(&target_addr, 0, sizeof(target_addr));
+
+    if (ping_args.host->count == 0)
+    {
+        //ping without host is used from sockets after on no activity to and from proxy
+        drv_console_set_log_enabled();
+        ESP_LOGI(TAG, "Ping on no activity from Proxy");
+        return 0;
+    }
+    else 
+    {
+        if (inet_pton(AF_INET6, ping_args.host->sval[0], &sock_addr6.sin6_addr) == 1) {
+            /* convert ip6 string to ip6 address */
+            ipaddr_aton(ping_args.host->sval[0], &target_addr);
+        } else {
+            struct addrinfo hint;
+            struct addrinfo *res = NULL;
+            memset(&hint, 0, sizeof(hint));
+            /* convert ip4 string or hostname to ip4 or ip6 address */
+            if (getaddrinfo(ping_args.host->sval[0], NULL, &hint, &res) != 0) {
+                printf("ping: unknown host %s\n", ping_args.host->sval[0]);
+                return 1;
+            }
+            if (res->ai_family == AF_INET) {
+                struct in_addr addr4 = ((struct sockaddr_in *) (res->ai_addr))->sin_addr;
+                inet_addr_to_ip4addr(ip_2_ip4(&target_addr), &addr4);
+            } else {
+                struct in6_addr addr6 = ((struct sockaddr_in6 *) (res->ai_addr))->sin6_addr;
+                inet6_addr_to_ip6addr(ip_2_ip6(&target_addr), &addr6);
+            }
+            freeaddrinfo(res);
+        }
+        config.target_addr = target_addr;
+
+        /* set callback functions */
+        esp_ping_callbacks_t cbs = {
+            .cb_args = NULL,
+            .on_ping_success = cmd_ping_on_ping_success,
+            .on_ping_timeout = cmd_ping_on_ping_timeout,
+            .on_ping_end = cmd_ping_on_ping_end
+        };
+        esp_ping_handle_t ping;
+        esp_ping_new_session(&config, &cbs, &ping);
+        esp_ping_start(ping);
+
+        return 0;
+
+    }
+}
+
+static void register_ping(void)
+{
+    ping_args.timeout = arg_dbl0("W", "timeout", "<t>", "Time to wait for a response, in seconds");
+    ping_args.interval = arg_dbl0("i", "interval", "<t>", "Wait interval seconds between sending each packet");
+    ping_args.data_size = arg_int0("s", "size", "<n>", "Specify the number of data bytes to be sent");
+    ping_args.count = arg_int0("c", "count", "<n>", "Stop after sending count packets");
+    ping_args.tos = arg_int0("Q", "tos", "<n>", "Set Type of Service related bits in IP datagrams");
+    ping_args.host = arg_str1(NULL, NULL, "<host>", "Host address");
+    ping_args.end = arg_end(0);
+    const esp_console_cmd_t ping_cmd = {
+        .command = "ping",
+        .help = "send ICMP ECHO_REQUEST to network hosts",
+        .hint = NULL,
+        .func = &do_ping_cmd,
+        .argtable = &ping_args
+    };
+    ESP_ERROR_CHECK(esp_console_cmd_register(&ping_cmd));
+}
+
+
+
+void cmd_ping_register(void)
+{
+    register_ping();
+}
